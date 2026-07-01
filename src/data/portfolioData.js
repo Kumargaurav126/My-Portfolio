3a2923ff1f1079d77d5f443c68fc97b89afb77e0
Kumargaurav126 <contact.kumargaurav12@gmail.com>
2026-07-01 21:42:52 +0530

    Change in Resume

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,5 +1,5 @@
 export const RESUME_LINK =
-  "https://drive.google.com/file/d/1inOATdpPAdm9JF7XAQG86622XbRE5Mzd/view?usp=sharing";
+  "https://drive.google.com/file/d/1g8qGmAkf_c_vO6TGIsNW-THlp6wkGFAz/view?usp=sharing";
 
 // ── Skills ────────────────────────────────────────────────────────────────────
 export const SKILLS = [
